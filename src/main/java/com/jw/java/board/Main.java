f074944944b7890974836ceb5f9a94e62e3ea59a
imujinu <wlsdnrhdwkd@gmail.com>
2025-03-07 15:08:39 +0900

    board

--- src/main/java/com/jw/java/board/Main.java
+++ src/main/java/com/jw/java/board/Main.java
@@ -9,8 +9,11 @@ public class Main {
         System.out.println("== 자바 텍트스 게시판 시작 == ");
         Scanner sc = new Scanner(System.in);
 
+            int id = 1;
         while (true){
-        System.out.println("명령)");
+
+
+        System.out.print("명령)");
         String cmd = sc.nextLine();
 
         if(cmd.equals("write")){
@@ -19,8 +22,14 @@ public class Main {
             String subject = sc.nextLine();
             System.out.print("내용 : ");
             String content = sc.nextLine();
-            int id = 1;
-            System.out.printf("%d번 게시물이 등록되었습니다. \n", id);
+            int number = id++;
+
+            Article article = new Article();
+            article.id = number;
+            article.subject = subject;
+            article.content = content;
+            System.out.println("생성 된 게시물 : " + article);
+            System.out.printf("%d번 게시물이 등록되었습니다. \n",  article.id);
         }
 
         else if(cmd.equals("exit")){
@@ -35,4 +44,14 @@ public class Main {
         System.out.println("== 자바 텍트스 게시판 끝 == ");
         sc.close();
     }
+
 }
+   class Article{
+    int id;
+    String subject;
+    String content;
+    @Override
+    public String toString(){
+        return "{id : %d, subject : \"%s\", content : \"%s\"}".formatted(id,subject,content);
+    }
+    }
