b33418d22417b2ae22c3812598f346fa5034f1df
imujinu <wlsdnrhdwkd@gmail.com>
2025-03-10 23:10:52 +0900

    orderBy

--- src/main/java/com/jw/java/board/Main.java
+++ src/main/java/com/jw/java/board/Main.java
@@ -37,16 +37,28 @@ public class Main {
             System.out.printf("%d번 게시물이 등록되었습니다. \n",  article.id);
             }
         else if(rq.getUrlPath().equals("/usr/article/list")){
+            System.out.println("==게시물 리스트==");
+            System.out.println("번호 | 제목");
+
+            List<Article> sortedArticles = articles;
+            Map <String, String> params = rq.getParams();
+            boolean orderByIdDesc = true;
+            if(params.containsKey("orderBy") && params.get("orderBy").equals("idAsc")){
+                orderByIdDesc = false;
+
+            }
+            if (orderByIdDesc){
+                sortedArticles = Util.reverseList(sortedArticles);
+
+            }
+            articles.forEach( article ->  System.out.printf("%d | %s\n", article.id , article.subject));
+
             if(articles.isEmpty()){
                 System.out.println("현재 게시물이 존재하지 않습니다.");
                 continue;
             }
-            System.out.println("번호 | 제목");
 
-            for(int i = articles.size()-1; i>= 0 ; i--){
-                Article article = articles.get(i);
-                System.out.printf("%d | %s\n", article.id , article.subject);
-            }
+
         }
 
         else if (rq.getUrlPath().equals("/usr/article/detail")) {
@@ -150,5 +162,15 @@ class Util{
 
     public static String getPathFromUrl(String url) {
         return url.split("\\?",2)[0];
+    }
+
+    public static <T> List<T> reverseList(List<T> list){
+        List<T> reverse = new ArrayList<>(list.size());
+
+        for (int i = list.size()-1; i>=0 ; i--){
+            reverse.add(list.get(i));
+        }
+
+        return reverse;
     }
 }
